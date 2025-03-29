7588d2b4ed126ee6bfa8a8a09e3b4428c10b3ad6
aserravalle <49396991+aserravalle@users.noreply.github.com>
2025-03-29 15:35:29 +0100

    infer missing data

--- app/main.py
+++ app/main.py
@@ -13,11 +13,19 @@ app = FastAPI(
 )
 app.include_router(scheduler.router)
 
-allow_origins = os.getenv("ALLOW_ORIGINS", "http://localhost:8080").split(",")
+# Configure CORS
+origins = [
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
+    os.getenv("ALLOW_ORIGINS", "").strip('"').split(","),  # Get production URL from .env and remove quotes
+]
+
+# Filter out empty strings from origins
+origins = [origin for origin in origins if origin]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=allow_origins,
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
