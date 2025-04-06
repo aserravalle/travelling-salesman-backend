87e99c7a954298a1e5dbd8942096a02419c1c631
aserravalle <49396991+aserravalle@users.noreply.github.com>
2025-04-06 14:53:39 +0200

    locationHelpers

--- app/models/location.py
+++ app/models/location.py
@@ -1,7 +1,8 @@
 from datetime import timedelta
 from typing import Optional
 from pydantic import BaseModel, Field, model_validator
-import requests
+
+from app.services.location_helpers import LocationHelpers
 
 class Location(BaseModel):
     """
@@ -28,7 +29,7 @@ class Location(BaseModel):
 
             # If no coordinates but address is present, generate coordinates
             if self.address is not None:
-                response = self.get_coordinates_from_address(self.address)
+                response = LocationHelpers.get_coordinates_from_address(self.address)
                 self.latitude = response.get('latitude')
                 self.longitude = response.get('longitude')
                 self.address = response.get('address')
@@ -57,38 +58,3 @@ class Location(BaseModel):
             return timedelta(minutes=20) # TODO: change once we implement GPS
 
         return timedelta(minutes=20)
-
-
-    @staticmethod
-    def get_coordinates_from_address(address: str) -> dict:
-        """
-        Get latitude, longitude (rounded to 4 decimals), and formatted address 
-        from a partial address using OpenStreetMap's Nominatim API.
-        Returns a dictionary with keys: 'latitude', 'longitude', 'address'.
-        """
-        try:
-            url = "https://nominatim.openstreetmap.org/search"
-            params = {
-                'q': address,
-                'format': 'json',
-                'addressdetails': 1,  # ask for extra details
-                'limit': 1            # only top result
-            }
-            headers = {
-                'User-Agent': 'TravellingSalesman/1.0 (magicchili1998@gmail.com)'  # Required
-            }
-            response = requests.get(url, params=params, headers=headers)
-            response.raise_for_status()
-            data = response.json()
-            if data:
-                result = data[0]
-                return {
-                    'latitude': round(float(result['lat']), 4),
-                    'longitude': round(float(result['lon']), 4),
-                    'address': result.get('display_name', '')
-                }
-            else:
-                return {}
-        except Exception as e:
-            print(f"Error getting coordinates for {address}: {e}")
-            raise e
